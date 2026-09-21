225708ba2323e1616c057d80d358778e378f1d2c
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2026-09-21 13:51:36 +0200

    Add heater inertia

--- room-simulator.py
+++ room-simulator.py
@@ -11,6 +11,7 @@ alpha2 = 0.1 # Coefficiente di scambio termico tra la stanza A e l'esterno
 alpha3 = 0.1 # Scambio termico tra le due stanze
 alpha4 = 0.002 # Quanto velocemente B reagisce ai cambiamenti
 alpha5 = 0.1 # Coefficiente di scambio termico tra la stanza B e l'esterno
+alpha6 = 0.001 # Velocita' di riscaldamento degli heater
 alpha7 = 1.0 # Potenza massima heater A
 alpha8 = 0.5 # Potenza massima heater B
 alpha9 = 0.1 # Efficienza del cooler
@@ -41,9 +42,8 @@ def get_cooler_power(t, actions):
 
     return alpha9 * current_power
 
-def get_heater_power(t, actions):
-    # Dato un tempo t, restituisce la potenza del riscaldatore
-    # guardando l'ultima azione eseguita prima di t
+def get_heater_ref(t, actions):
+    # Dato un tempo t, verifica se heater e' accesso o spento
     current_state = False # Stato iniziale
 
     for action_time, heater, cooler in actions:
@@ -52,24 +52,24 @@ def get_heater_power(t, actions):
         else:
             break
 
-    return H_max if current_state else 0.0
+    return 1.0 if current_state else 0.0
 
 # Equazione differenziale
 def deriv(t, y):
     T_A = y[0] # Temperatura corrente stanza A
-    T_B = y[1] # Temperatura corrente stanza B
+    T_B = y[1] # Temperatura corrente stanza B    
+    H_A = y[2] # Potenza del riscaldatore A al tempo t
+    H_B = y[3] # Potenza del riscaldatore B al tempo t
     
-    heater_on = get_heater_power(t, actions) # Restituisce se l'heater e' acceso o spento (1 o 0)
-    
-    H_A = alpha7 * heater_on # Potenza del riscaldatore A al tempo t
-    H_B = alpha8 * heater_on # Potenza del riscaldatore B al tempo t
-    
+    heater_ref = get_heater_ref(t, actions) # 1 o 0
     C = get_cooler_power(t, actions) # Potenza del cooler al tempo t
     
     dTdt_A = alpha1 * (alpha2 * (T_out - T_A) + H_A - C + alpha3 * (T_B - T_A)) # Differenza di temperatura al secondo in questo istante nella stanza A
     dTdt_B = alpha4 * (alpha5 * (T_out - T_B) + H_B + alpha3 * (T_A - T_B)) # Differenza di temperatura al secondo in questo istante nella stanza B
+    dHdt_A = alpha6 *  (alpha7 * heater_ref - H_A) # Inerzia di heater A
+    dHdt_B = alpha6 *  (alpha8 * heater_ref - H_B) # Inerzia di heater B
 
-    return [dTdt_A, dTdt_B]
+    return [dTdt_A, dTdt_B, dHdt_A, dHdt_B]
 
 #Condizioni iniziali
 T_A_initial = 10.0 
@@ -83,7 +83,7 @@ t_eval = np.arange(t_start, t_end, 60) # Punti in cui si vuole conoscere la temp
 solution = solve_ivp(
     fun = deriv, # Funzione che calcola le derivate
     t_span = [t_start, t_end], # Intervallo di tempo
-    y0 = [T_A_initial, T_B_initial], 
+    y0 = [T_A_initial, T_B_initial, 0, 0], 
     t_eval = t_eval,
     method = 'RK45'
 )
@@ -91,6 +91,9 @@ solution = solve_ivp(
 times = solution.t  # Array dei tempi
 temp_A = solution.y[0]  # Temperature stanza A
 temp_B = solution.y[1]  # Temperature stanza B
+heat_A = solution.y[2]  # Potenza effettiva heater A nel tempo
+heat_B = solution.y[3]  # Potenza effettiva heater B nel tempo
+
 
 # Aggiunta del rumore
 sigma = 0.1
@@ -98,7 +101,7 @@ measured_A_temp = temp_A + np.random.normal(0, sigma, size = len(temp_A))
 measured_B_temp = temp_B + np.random.normal(0, sigma, size = len(temp_B))
 
 # Restituisce la potenza del riscaldatore ad ogni momento
-heater_state = np.array([get_heater_power(t, actions) for t in times])
+heater_state = np.array([get_heater_ref(t, actions) for t in times])
 cooler_state = np.array([get_cooler_power(t, actions) for t in times])
 
 hour = times / 3600
@@ -110,7 +113,7 @@ print(f"{'Hour':>6} | {'T A real':>10} | {'T A Measured':>10} | {'T B real':>10}
 for step in time_step:
     idx = np.argmin(np.abs(times - step))
     hour_stamp = step / 3600
-    heater = "ON" if get_heater_power(step, actions) > 0 else "OFF"
+    heater = "ON" if get_heater_ref(step, actions) > 0 else "OFF"
     cooler_val = get_cooler_power(step, actions) / alpha9
     print(f"{hour_stamp:5.1f}h | {temp_A[idx]:9.4f} C | {measured_A_temp[idx]:9.4f} C | {temp_B[idx]:9.4f} C | {measured_B_temp[idx]:9.4f} C  | {heater:>12} | {cooler_val:>8.0f}/9")
 
